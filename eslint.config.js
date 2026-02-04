d5eb66b7e86add144f99c5664030626f4a8cd9a6
nikiforov <l760@ya.ru>
2026-02-04 21:43:21 +0500

    Настройка шаблона vue

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginVue from 'eslint-plugin-vue';
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default defineConfig(
+  { ignores: ['*.d.ts', '**/dist'] },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+    rules: {
+      // your rules
+    },
+  },
+  eslintPluginPrettierRecommended
+);
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>MainPage</div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
